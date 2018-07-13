c833c695856bafda6d78093d29a0c8b306a44a75
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-13 14:35:26 -0700

    Create a RepoListEntry component

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-
-const RepoList = (props) => (
-  <div>
-    <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
-  </div>
-)
-
+//attempt to import: import RepoListEntry from '/RepoListEntry.jsx';
+const RepoList = (props) => {
+  var repos = props.repos;
+  console.log(repos);
+  return (
+    <div>
+      <h4> Repo List Component </h4>
+      There are {props.repos.length} repos.
+     {/*HERE IS HOW VARIOUS REPOLISTNETRIES WOULD BE RENDERED {repos.map((repo) => <RepoListEntry repo = {repo}/>)} */}
+    </div>
+  )
+}
 export default RepoList;
---
+++ client/src/components/RepoListEntry.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+var RepoListEntry = (props) => {
+  //var repo = props.repo;
+  return (
+  <div>
+  <h4>Here is a repo list entry</h4>
+  </div>
+  )
+}
+
+export default RepoListEntry;
